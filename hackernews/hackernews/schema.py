bce3ff95b4793c15c082036ca87480f93d3fefe3
Daniel Torres <daniel.torres@liftit.co>
2018-11-09 17:07:28 -0500

    Creating relay implementation to links

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -4,12 +4,17 @@ import graphql_jwt
 
 import links.schema
 import users.schema
+import links.schema_relay
 
-
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    users.schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType,
+):
     pass
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType, links.schema_relay.RelayMutation):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field() # -> Mutation to authenticate users
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
